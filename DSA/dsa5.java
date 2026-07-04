3afca2fd9d1c9cb30cffef4111e8a7e7445a05f6
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-07-05 00:39:51 +0530

    Create dsa5.java

---
+++ DSA/dsa5.java
@@ -0,0 +1,18 @@
+public class dsa5{
+    public static void main(String[] args){
+        String st = "welcomeToScaler";
+        removeVowels(st);
+    }
+
+    public static void removeVowels(String st){
+            for(int i = 0; i < st.length(); i++){
+                char ch = st.charAt(i);
+
+                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                    continue;
+                }
+
+                System.out.print(ch);
+            }
+        }
+}
