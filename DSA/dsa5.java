2e9d7bf35c909e06772382c2fc155e2ad70f8128
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-07-05 00:55:47 +0530

    Update dsa5.java

--- DSA/dsa5.java
+++ DSA/dsa5.java
@@ -1,19 +1,61 @@
-public class dsa5{
-    
+public class dsa5 {
+
     public static void main(String[] args){
+
         String st = "welcomeToScaler";
+
         removeVowels(st);
+
+        System.out.println();
+
+        reverseVowels(st);
     }
 
     public static void removeVowels(String st){
-            for(int i = 0; i < st.length(); i++){
-                char ch = st.charAt(i);
 
-                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-                    continue;
-                }
+        for(int i=0;i<st.length();i++){
 
-                System.out.print(ch);
-            }
+            char ch = st.charAt(i);
+
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+                continue;
+
+            System.out.print(ch);
         }
+    }
+
+    public static boolean isVowel(char ch){
+
+        return ch=='a'||
+               ch=='e'||
+               ch=='i'||
+               ch=='o'||
+               ch=='u';
+    }
+
+    public static void reverseVowels(String st){
+
+        char[] s = st.toCharArray();
+
+        int i=0;
+        int j=s.length-1;
+
+        while(i<j){
+
+            while(i<j && !isVowel(s[i]))
+                i++;
+
+            while(i<j && !isVowel(s[j]))
+                j--;
+
+            char temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
+
+            i++;
+            j--;
+        }
+
+        System.out.println(new String(s));
+    }
 }
