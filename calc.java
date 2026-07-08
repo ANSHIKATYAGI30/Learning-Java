7eda979622811b4411215c57d13dac0b4158f81d
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-07-08 13:49:03 +0530

    Update calc.java

--- calc.java
+++ calc.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 public class calc {
+    
     public static void main(String[] args){
+        
         Scanner sc = new Scanner(System.in);
         int p = sc.nextInt();
         int r = sc.nextInt();
