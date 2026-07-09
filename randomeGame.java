9e7fcfd8ce725d570229ad4cf25ba976957dc9ba
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-07-09 12:18:34 +0530

    Update randomeGame.java

--- randomeGame.java
+++ randomeGame.java
@@ -1,6 +1,9 @@
 public class randomeGame {
+    
     public static void main(String[] args){
+        
         while(true){
+            
             int num = (int) (Math.random() * 100 + 1);
             if(num == 5 || num == 50){break;}
             System.out.println(num + " ");
