248cf0ac5fb1a77a286d6f546dc1bcfe2d44e869
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-07-11 15:28:20 +0530

    Update dsa13.java

--- DSA/dsa13.java
+++ DSA/dsa13.java
@@ -1,1 +1,25 @@
+//queue-operations
+//"first in first out"
 
+public class dsa13{
+  public static void main(String[] args){
+    Queue<Integer> queue = new ArrayDeque<>();
+
+    //add
+    queue.offer(10);
+    queue.offer(20);
+    queue.offer(80);
+    queue.offer(120);
+    queue.offer(180);
+
+    //get the front element
+    System.out.println(queue.peek());
+
+    //remove element
+    System.out.println(queue.remove());
+    System.out.println(queue.peek());
+
+    //size
+    System.out.println(queue.size());
+  }
+}
