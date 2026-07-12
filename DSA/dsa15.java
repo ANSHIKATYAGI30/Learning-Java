10a11dd4553bdca9b4f9efbc2b0ab5f58dc7686c
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-07-12 15:21:51 +0530

    Create dsa15.java

---
+++ DSA/dsa15.java
@@ -0,0 +1,15 @@
+public class dsa15{
+
+  class DNode{
+    int data; 
+    DNode prev;
+    DNode next;
+
+    DNode(int data){
+      this.data = data;
+    }
+  }
+  public static void main(String[] args){
+    
+  }
+}
