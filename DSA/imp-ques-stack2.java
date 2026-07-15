796ce358fabfa8a470e4891302e23b7c82d93274
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-07-15 13:55:47 +0530

    Update imp-ques-stack2.java

--- DSA/imp-ques-stack2.java
+++ DSA/imp-ques-stack2.java
@@ -54,57 +54,39 @@ public class Stack_Level2 {
         return ans;
     }
   
-    // ============================================================
+
     // 4. PREVIOUS SMALLER ELEMENT
-    // ============================================================
-
     public static int[] previousSmaller(int[] arr) {
-
         int n = arr.length;
         int[] ans = new int[n];
-
         Stack<Integer> st = new Stack<>();
-
         for (int i = 0; i < n; i++) {
-
             while (!st.isEmpty() && st.peek() >= arr[i])
                 st.pop();
-
             if (st.isEmpty())
                 ans[i] = -1;
             else
                 ans[i] = st.peek();
-
             st.push(arr[i]);
         }
-
         return ans;
     }
 
-    // ============================================================
+   
     // 5. STOCK SPAN
-    // ============================================================
-
     public static int[] stockSpan(int[] price) {
-
         int n = price.length;
         int[] span = new int[n];
-
         Stack<Integer> st = new Stack<>();
-
         for (int i = 0; i < n; i++) {
-
             while (!st.isEmpty() && price[st.peek()] <= price[i])
                 st.pop();
-
             if (st.isEmpty())
                 span[i] = i + 1;
             else
                 span[i] = i - st.peek();
-
             st.push(i);
         }
-
         return span;
     }
 
