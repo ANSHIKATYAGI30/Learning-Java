31d7669a553e46d1a962160755e3578b2b0dd59a
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-07-15 13:53:19 +0530

    Update imp-ques-stack2.java

--- DSA/imp-ques-stack2.java
+++ DSA/imp-ques-stack2.java
@@ -19,60 +19,41 @@ public class Stack_Level2 {
         return ans;
     }
 
-    // ============================================================
+    
     // 2. PREVIOUS GREATER ELEMENT
-    // ============================================================
-
     public static int[] previousGreater(int[] arr) {
-
         int n = arr.length;
         int[] ans = new int[n];
-
         Stack<Integer> st = new Stack<>();
-
         for (int i = 0; i < n; i++) {
-
             while (!st.isEmpty() && st.peek() <= arr[i])
                 st.pop();
-
             if (st.isEmpty())
                 ans[i] = -1;
             else
                 ans[i] = st.peek();
-
             st.push(arr[i]);
         }
-
         return ans;
     }
 
-    // ============================================================
     // 3. NEXT SMALLER ELEMENT
-    // ============================================================
-
     public static int[] nextSmaller(int[] arr) {
-
         int n = arr.length;
         int[] ans = new int[n];
-
         Stack<Integer> st = new Stack<>();
-
         for (int i = n - 1; i >= 0; i--) {
-
             while (!st.isEmpty() && st.peek() >= arr[i])
                 st.pop();
-
             if (st.isEmpty())
                 ans[i] = -1;
             else
                 ans[i] = st.peek();
-
             st.push(arr[i]);
         }
-
         return ans;
     }
-
+  
     // ============================================================
     // 4. PREVIOUS SMALLER ELEMENT
     // ============================================================
