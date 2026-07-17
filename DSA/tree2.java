18150c7e4264e029a4093afbcd6094d14d8a08e4
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-07-17 22:21:11 +0530

    Update tree2.java

--- DSA/tree2.java
+++ DSA/tree2.java
@@ -33,5 +33,18 @@ public class tree2{
   }
 
   //mirror tree
+  static void mirror(Node root){
+    if(root == null){
+      return;
+    }
+    Node temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    mirror(root.left);
+    mirror(root.right);
+  }
+
+  //identical trees
   
 }
