b04b9fad0d5358cb2d2f2fc764e3c91024512b5b
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-07-19 12:20:37 +0530

    Update binaryTree.java

--- DSA/binaryTree.java
+++ DSA/binaryTree.java
@@ -28,3 +28,17 @@ public class binaryTree{
         return root;
     }
 
+    // SEARCH
+    static boolean search(Node root, int key) {
+        if (root == null)
+            return false;
+
+        if (root.data == key)
+            return true;
+
+        if (key < root.data)
+            return search(root.left, key);
+
+        return search(root.right, key);
+    }
+
