8cd3bd8d8887d6014d5026d8128d95781d2802f4
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-07-19 11:49:10 +0530

    Create binaryTree.java

---
+++ DSA/binaryTree.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class binaryTree{
+  
+    // Node Structure
+    static class Node {
+
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+        }
+}
