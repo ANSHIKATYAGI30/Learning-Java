9f36390d67b751d66d2bf2822c29f06c7c43894e
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-07-19 12:35:39 +0530

    Update binaryTree.java

--- DSA/binaryTree.java
+++ DSA/binaryTree.java
@@ -4,10 +4,8 @@ public class binaryTree{
   
     // Node Structure
     static class Node {
-
         int data;
         Node left, right;
-
         Node(int data) {
             this.data = data;
         }
