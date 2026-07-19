a93f0066e80b4c402dba26703298a494ec4a433c
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-07-19 12:13:32 +0530

    Update binaryTree.java

--- DSA/binaryTree.java
+++ DSA/binaryTree.java
@@ -12,3 +12,19 @@ public class binaryTree{
             this.data = data;
         }
 }
+  
+    // INSERT
+    static Node insert(Node root, int val) {
+
+        if (root == null){
+          return new Node(val);
+        }
+        if (val < root.data){
+            root.left = insert(root.left, val);
+        }
+        else{
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
