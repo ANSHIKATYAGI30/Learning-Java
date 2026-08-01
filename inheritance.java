91c89ccadf3d28c9b56157a17a4a221ddec4d8f5
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-08-02 00:56:38 +0530

    Update inheritance.java

--- inheritance.java
+++ inheritance.java
@@ -1,55 +1,86 @@
-class Person{
+class Date {
+
+    int day, month, year;
+
+    void printDate() {
+        System.out.println(day + "/" + month + "/" + year);
+    }
+}
+
+class Person {
+
     String name;
     Date dob;
     int mobileNo;
-    void readData(String s, Date d, int m){
+
+    void readData(String s, Date d, int m) {
+
         name = s;
         dob = d;
         mobileNo = m;
     }
-    void printData(){
-        System.out.println("Name: ", + name);
+
+    void printData() {
+
+        System.out.println("Name : " + name);
         dob.printDate();
-        System.out.println("Mobile: ", + mobileNo);
+        System.out.println("Mobile : " + mobileNo);
     }
 }
 
-class Student extends Person{
+class Student extends Person {
+
     String institution;
     int[] qualif = new int[20];
     int rollNo;
     int[] marks = new int[5];
 
-    void printBiodata(){
+    void printBiodata() {
+
         printData();
-        Sytem.out.println("Institution: " + institution);
-        System.out.println("Roll: " + rollNo);
-        for(int q = 0; q < qualif.length; q++){
-            System.out.println("Marks: " + q + qualif[q]);
+
+        System.out.println("Institution : " + institution);
+        System.out.println("Roll No : " + rollNo);
+
+        for (int i = 0; i < qualif.length; i++) {
+            System.out.println("Qualification " + i + " : " + qualif[i]);
         }
-        for(int m = 0; m < marks.length; m++){
-            System.out.println("Result: " + m + marks[m]);
+
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println("Marks " + i + " : " + marks[i]);
         }
     }
 }
 
-class Employee extends Person{
+class Employee extends Person {
+
     int empNo;
     int[] salaryHistory = new int[12];
     String organization;
     String designation;
-    Date dob;
-    void printSalary(){
-        for(int p = 0; p < salaryHistory.length; p++){
-            System.out.println("Salary " + p + " : " + salaryHistory[p]);
+
+    void printSalary() {
+
+        for (int i = 0; i < salaryHistory.length; i++) {
+            System.out.println("Salary " + i + " : " + salaryHistory[i]);
         }
     }
 }
 
-class inheritance{
-    public static void main(String args[]){
+public class inheritance {
+
+    public static void main(String[] args) {
+
         Person p = new Person();
-        Student s = new Student[100];
-        Emplyee e = new Employee[50];
+
+        Student s = new Student();
+
+        Employee e = new Employee();
+
+        // OR if you want arrays
+
+        Student[] students = new Student[100];
+
+        Employee[] employees = new Employee[50];
     }
 }
