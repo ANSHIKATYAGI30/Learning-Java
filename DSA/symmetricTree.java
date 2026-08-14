3a81a401c4239ac58bbcc8b680860dab30f65f6b
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-08-14 07:06:49 +0530

    Create symmetricTree.java

---
+++ DSA/symmetricTree.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return mirror(root.left, root.right);
+    }
+
+    boolean mirror(TreeNode left, TreeNode right) {
+
+        // Both are empty
+        if (left == null && right == null)
+            return true;
+
+        // One is empty, other is not
+        if (left == null || right == null)
+            return false;
+
+        // Values must be equal
+        if (left.val != right.val)
+            return false;
+
+        // Check mirror pairs
+        return mirror(left.left, right.right) &&
+               mirror(left.right, right.left);
+    }
+}
