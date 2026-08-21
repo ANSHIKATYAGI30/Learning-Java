c411db94a307e8dc9521e318205ec8f7b417e02d
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-08-21 08:49:51 +0530

    Update inheritance4.java

--- inheritance4.java
+++ inheritance4.java
@@ -1,13 +1,12 @@
 //super.X
 class Father{
-	
 	int fdata=10;
 	Father(){System.out.println("In Father class");}
 	void displayFData() {
 		System.out.println("fdata="+fdata);
 	}
-	
 }
+
 class Child extends Father{
 	int cdata=20;
 	int X=100;
@@ -16,11 +15,10 @@ class Child extends Father{
 		super();
 		System.out.println("In Child class");}
 	void displayCData() {
-		System.out.println("cdata="+cdata);
-		
+		System.out.println("cdata="+cdata);	
 	}
-	
 }
+
 class GrandChild extends Child{
 	int gcdata=30;
 	int X=200;
@@ -28,11 +26,9 @@ class GrandChild extends Child{
 		super();//will be written by compiler if not written in code
 		System.out.println("In GrandChild");}
 	void displayGCData() {
-		
 		System.out.println("gcdata="+gcdata);
 		displayCData();
 		displayFData();
-		
 	}
 	void displayX() {
 		int X=900;
@@ -41,13 +37,12 @@ class GrandChild extends Child{
 		System.out.println("X="+super.X);//100 instance of parent class
 	}
 }
-public class inheritance4 {
 
+public class inheritance4 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		GrandChild ob1=new GrandChild();
-		//GrandChild ob2=new GrandChild();
-		//ob1.displayGCData();
+		GrandChild ob2=new GrandChild();
+		ob1.displayGCData();
 		ob1.displayX();
 		
 	}
