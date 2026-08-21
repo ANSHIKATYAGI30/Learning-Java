608e9bbcefde84c920b89a9e00b752710472b6d5
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-08-21 08:47:27 +0530

    Update inheritance1.java

--- inheritance1.java
+++ inheritance1.java
@@ -11,21 +11,19 @@ class Child extends Father{
 		System.out.println("cdata="+cdata);
 	}
 }
+
 class GrandChild extends Child{
 	int gcdata=30;
 	void displayGCData() {
 		System.out.println("gcdata="+gcdata);
 	}
 }
-public class inheritance1 {
 
+public class inheritance1 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		GrandChild ob1=new GrandChild();
 		ob1.displayGCData();
 		ob1.displayCData();
 		ob1.dislayFData();
-		
 	}
-
 }
