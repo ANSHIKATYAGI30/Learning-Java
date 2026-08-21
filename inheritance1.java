597f96bebc4899b555d42595cd5bf6b31d9fbf09
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-08-21 08:46:35 +0530

    Add files via upload

--- inheritance1.java
+++ inheritance1.java
@@ -1,6 +1,6 @@
 class Father{
 	int fdata=10;
-	void displayFData() {
+	void dislayFData() {
 		System.out.println("fdata="+fdata);
 	}
 	
@@ -9,26 +9,23 @@ class Child extends Father{
 	int cdata=20;
 	void displayCData() {
 		System.out.println("cdata="+cdata);
-		
 	}
 	
 }
 class GrandChild extends Child{
 	int gcdata=30;
-	
 	void displayGCData() {
 		System.out.println("gcdata="+gcdata);
-		displayCData();
-		displayFData();
-		
 	}
 }
-public class inheritance2 {
+public class inheritance1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GrandChild ob1=new GrandChild();
 		ob1.displayGCData();
+		ob1.displayCData();
+		ob1.dislayFData();
 		
 	}
 
--- inheritance4.java
+++ inheritance4.java
@@ -1,5 +1,6 @@
-//constructor chaining
+//super.X
 class Father{
+	
 	int fdata=10;
 	Father(){System.out.println("In Father class");}
 	void displayFData() {
@@ -9,6 +10,8 @@ class Father{
 }
 class Child extends Father{
 	int cdata=20;
+	int X=100;
+	int Y=70;
 	Child(){
 		super();
 		System.out.println("In Child class");}
@@ -20,25 +23,35 @@ class Child extends Father{
 }
 class GrandChild extends Child{
 	int gcdata=30;
+	int X=200;
 	GrandChild(){
 		super();//will be written by compiler if not written in code
 		System.out.println("In GrandChild");}
 	void displayGCData() {
+		
 		System.out.println("gcdata="+gcdata);
 		displayCData();
 		displayFData();
 		
 	}
+	void displayX() {
+		int X=900;
+		System.out.println("X="+X);//900 local
+		System.out.println("X="+this.X);//200 instance
+		System.out.println("X="+super.X);//100 instance of parent class
+	}
 }
-public class inheritance3 {
+public class inheritance4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GrandChild ob1=new GrandChild();
-		GrandChild ob2=new GrandChild();
-		ob1.displayGCData();
+		//GrandChild ob2=new GrandChild();
+		//ob1.displayGCData();
+		ob1.displayX();
 		
 	}
 
 }
-//super()
+//this()    this.x
+//super()   super.x
