f71cdd78c5d76efa5b5f064c2c6a9f5e199d0899
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-08-22 22:15:15 +0530

    Update inheritance4.java

--- inheritance4.java
+++ inheritance4.java
@@ -23,7 +23,7 @@ class GrandChild extends Child{
 	int gcdata=30;
 	int X=200;
 	GrandChild(){
-		super();//will be written by compiler if not written in code
+		super(); //will be written by compiler if not written in code
 		System.out.println("In GrandChild");}
 	void displayGCData() {
 		System.out.println("gcdata="+gcdata);
